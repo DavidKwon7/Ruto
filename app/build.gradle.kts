509db29ef2f2410a44be94e0ed7b8a313343764c
DavidKwon7 <khjunejune10@gmail.com>
2025-11-29 22:56:13 +0900

    [release] 1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "com.handylab.ruto"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
